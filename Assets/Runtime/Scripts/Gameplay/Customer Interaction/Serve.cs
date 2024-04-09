7999cbb1cb80378382c3d58c4952462abe1c1176
CharlieB02 <charliebycroft@gmail.com>
2024-04-09 11:55:58 +0100

    Start of ID asigning to drinks, list check works

--- Assets/Runtime/Scripts/Gameplay/Customer Interaction/Serve.cs
+++ Assets/Runtime/Scripts/Gameplay/Customer Interaction/Serve.cs
@@ -19,37 +19,6 @@ public class Serve : MonoBehaviour
 
     void Update()
     {
-        //if (serveID == 1)  //Keep temporarily incase new version doesn't function properly
-        //{
-        //    for (int i = 0; i < Order.customerID.Length; i++)
-        //    {
-        //        if (Order.drinkID[i] == 1)
-        //        {
-        //            Debug.Log("Customer served");
-        //            Order.drinkID[i] = 0;
-        //            Order.customerID[i] = 0;
-        //            Destroy(Order.slots[i].transform.GetChild(0).gameObject);
-        //            serveID = 0;
-        //            break;
-        //        }
-        //    }
-        //}
-        //else if (serveID == 2)
-        //{
-        //    for (int i = 0; i < Order.customerID.Length; i++)
-        //    {
-        //        if (Order.drinkID[i] == 2)
-        //        {
-        //            Debug.Log("Customer served");
-        //            Order.drinkID[i] = 0;
-        //            Order.customerID[i] = 0;
-        //            Destroy(Order.slots[i].transform.GetChild(0).gameObject);
-        //            serveID = 0;
-        //            break;
-        //        }
-        //    }
-        //}
-
         newTime = MainTimer.GetComponent<Timer>().timer;
 
         for (int i = 0; i < Order.customerID.Length; i++)
--- Assets/Runtime/Scripts/Gameplay/Interactables/Mug.cs
+++ Assets/Runtime/Scripts/Gameplay/Interactables/Mug.cs
@@ -1,17 +1,30 @@
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
 
 public class Mug : Item {
     public List<IngredientType> ingredients = new List<IngredientType>();
     public bool IsDirty { get; private set; } = true;
 
+    public List<IngredientType> DrinkType1 = new List<IngredientType>();
+
     public void AddIngredient(IngredientType ingredient) {
         if (!ingredients.Contains(ingredient)) {
             ingredients.Add(ingredient);
         }
+        AsignDrinkID();
     }
 
     public void Clean() {
         IsDirty = false;
+    }
+
+    //checks the ingredient list and asigns an ID if it matches a predetermined list
+    public void AsignDrinkID()
+    {
+        if (ingredients.SequenceEqual(DrinkType1))
+        {
+            UnityEngine.Debug.Log("HELLO");
+        }
     }
 }
